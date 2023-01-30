fba980f851a686d45ed071ede21e0b8e4dd9f68e
hnnav <57915266+hnnav@users.noreply.github.com>
2023-01-30 13:51:51 +0200

    params validation

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ form.addEventListener("submit", (e) => {
   // Get url value
   const newUrl = new URL(input.value);
 
-  Client.displayData(newUrl.url, newUrl.validateScheme(), newUrl.validatePath());
+  Client.displayData(newUrl.url, newUrl.validateScheme(), newUrl.validatePath(), newUrl.validateParams());
   Client.clearInput();
 });
 
@@ -32,7 +32,7 @@ class URL {
 
   validatePath(){
     const path = this.url.substring(this.url.lastIndexOf('/') + 1, this.url.indexOf('?'));
-    console.log(path);
+
     if (path === 'login' || 
         path === 'confirm' || 
         path === 'sign') {
@@ -43,40 +43,52 @@ class URL {
   }
 
   validateParams(){ // Must return params as key: value pairs
+    const path = this.url.substring(this.url.lastIndexOf('/') + 1, this.url.indexOf('?'));
+
+    const searchParams = new URLSearchParams(this.url.substring(this.url.indexOf('?')));
+
+    const paramsObj = {};
+    for (const [key, value] of searchParams.entries()) {
+      paramsObj[key] = value;
+    }
 
     // Login: source(type:string)
-
+    if ((path === 'login') && (typeof paramsObj.source === 'string')) {
+      return `Valid parameters for login path: source= ${paramsObj.source}`;
+    }
     // Confirm: source(type:string), payment number(type:integer)
-
+    else if ((path === 'confirm') && (typeof paramsObj.source === 'string') && (/^\d+$/.test(paramsObj.paymentnumber) === true)) {
+      return `Valid parameters for confirm path: source= ${paramsObj.source}, payment number= ${paramsObj.paymentnumber}`;
+    }
     // Sign: source(type: string), documentid(type:string)
+    else if ((path === 'sign') && (typeof paramsObj.documentid === 'string')) {
+      return `Valid parameters for sign path: source= ${paramsObj.source}, document id= ${paramsObj.documentid}`;
+    } else {
+      return 'Incorrect parameters';
+    }
 
   }
 }
 
 class Client {
 
-  static displayData(url, scheme, path){
+  static displayData(url, scheme, path, params){
 
     // Create elements
     const newDiv = document.createElement("div");
-    newDiv.setAttribute('class', 'url-container');
-
-    const UrlDisplay = document.createElement("p");
-    UrlDisplay.setAttribute('class', 'url-display');
-
+    const UrlDisplay = document.createElement("h3");
     const schemeDisplay = document.createElement("p");
-    schemeDisplay.setAttribute('class', 'scheme-display');
-
     const pathDisplay = document.createElement("p");
-    pathDisplay.setAttribute('class', 'path-display');
+    const paramDisplay = document.createElement("p");
 
     // Add values
     UrlDisplay.innerHTML = 'URL: ' + url
     schemeDisplay.innerHTML = scheme
     pathDisplay.innerHTML = path
+    paramDisplay.innerHTML = params
 
     // Add to page
-    newDiv.append(UrlDisplay, schemeDisplay, pathDisplay)
+    newDiv.append(UrlDisplay, schemeDisplay, pathDisplay, paramDisplay)
     urlContainer.append(newDiv)
   }
 
